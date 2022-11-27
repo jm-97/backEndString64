ddfac8c1de247bb72bb9f893b696966214e14429
Manuico Huamani Jose Richard <j.manuicohuamani@reply.it>
2022-11-28 00:11:21 +0100

    mail works , addeb json body handler

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
 const express = require('express')
+
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 const app = express()
 const hostname = '127.0.0.1';
 const port = 3000;
+var nodemailer = require('nodemailer');
 
-app.get('/', (req, res) => {
-    res.json({a:1})
+var transporter = nodemailer.createTransport({
+  service: 'hotmail',
+  auth: {
+    user: 'jose.manuico@outlook.it',
+    pass: 'manujose02'
+  }
+});
+
+
+
+
+app.post('/sendMail',jsonParser, (req, res) => {
+  console.log('req.body',req.body.msg)
+  let mailOptions = {
+    from: 'jose.manuico@outlook.it',
+    to: 'manujose897@gmail.com',
+    subject: 'Base64',
+    text: req.body.msg
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      res.json({a:error})
+    } else {
+      console.log('Email sent: ' + info.response);
+      
+    res.json({info:info.response})
+    }
+  });
   })
   
   app.listen(port, () => {
