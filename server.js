0b5bea9fd02795fb5a46d265e1ef379783de3343
Manuico Huamani Jose Richard <j.manuicohuamani@reply.it>
2022-12-15 23:42:28 +0100

    cors

--- server.js
+++ server.js
@@ -14,7 +14,17 @@ var transporter = nodemailer.createTransport({
   }
 });
 
-
+app.use((req, res, next) => {
+  //allow access from every, elminate CORS
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.removeHeader('x-powered-by');
+  //set the allowed HTTP methods to be requested
+  res.setHeader('Access-Control-Allow-Methods','POST');
+  //headers clients can use in their requests
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+  //allow request to continue and be handled by routes
+  next();
+});
 
 
 app.post('/sendMail',jsonParser, (req, res) => {
